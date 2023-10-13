41b838df7f90da678f9d217582e579c9b2d5f1ec
aishatrue <140340401+aishatrue@users.noreply.github.com>
2023-10-13 23:49:27 +0900

    color choose painting

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const canvas = document.querySelector("canvas");
-
+const default_button = document.querySelector(".default");
+const first_button = document.querySelector(".first");
+const second_button = document.querySelector(".second");
+const third_button = document.querySelector(".third");
+const fourth_button = document.querySelector(".fourth");
+const eraser = document.querySelector(".eraser");
 // 캔버스에 그림을 그릴때 사용하는 것. canvas는 html의 한 element
 const ctx = canvas.getContext("2d");
+
+colors = ["#fc5c65", "#fd9644", "#fed330", "#26de81"];
 
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
 let isPainting = false;
+let currentColor = "black";
 
 function onMove(event) {
   if (isPainting) {
+    ctx.strokeStyle = currentColor;
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
@@ -34,3 +43,30 @@ canvas.addEventListener("mouseup", cancelPainting);
 // document.addEventListener("mouseup", onMouseUp);
 
 canvas.addEventListener("mouseleave", cancelPainting);
+
+function choseColor(event) {
+  let currentClass = event.target.className;
+
+  if (currentClass == "first") {
+    currentColor = colors[0];
+  } else if (currentClass == "second") {
+    currentColor = colors[1];
+  } else if (currentClass == "third") {
+    currentColor = colors[2];
+  } else if (currentClass == "fourth") {
+    currentColor = colors[3];
+  } else if (currentClass == "default") {
+    currentColor = "black";
+  } else {
+    currentColor = "white";
+  }
+
+  ctx.beginPath();
+}
+
+first_button.addEventListener("click", choseColor);
+second_button.addEventListener("click", choseColor);
+third_button.addEventListener("click", choseColor);
+fourth_button.addEventListener("click", choseColor);
+default_button.addEventListener("click", choseColor);
+eraser.addEventListener("click", choseColor);
