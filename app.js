4e6fbfc8465d06de8fd9dc965c655539e85fa619
aishatrue <140340401+aishatrue@users.noreply.github.com>
2023-10-15 21:52:55 +0900

    nico color choosing

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const canvas = document.querySelector("canvas");
 const linewidth = document.getElementById("line-width");
-const default_button = document.querySelector(".default");
-const first_button = document.querySelector(".first");
-const second_button = document.querySelector(".second");
-const third_button = document.querySelector(".third");
-const fourth_button = document.querySelector(".fourth");
-const eraser = document.querySelector(".eraser");
+const color = document.getElementById("color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 // 캔버스에 그림을 그릴때 사용하는 것. canvas는 html의 한 element
 const ctx = canvas.getContext("2d");
-
-colors = ["#fc5c65", "#fd9644", "#fed330", "#26de81"];
 
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = linewidth.value;
 
 let isPainting = false;
-let currentColor = "black";
 
 function onMove(event) {
   if (isPainting) {
-    ctx.strokeStyle = currentColor;
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
@@ -42,6 +36,17 @@ function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
+// 이거 한번에 다 바꾸도록 만들어보기
+function onColorChange(event) {
+  ctx.strokeStyle = ctx.fillStyle = event.target.value;
+}
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
+
+  // 선택한 색깔 알려주기 위해
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -50,31 +55,7 @@ canvas.addEventListener("mouseup", cancelPainting);
 
 canvas.addEventListener("mouseleave", cancelPainting);
 
-function choseColor(event) {
-  let currentClass = event.target.className;
-
-  if (currentClass == "first") {
-    currentColor = colors[0];
-  } else if (currentClass == "second") {
-    currentColor = colors[1];
-  } else if (currentClass == "third") {
-    currentColor = colors[2];
-  } else if (currentClass == "fourth") {
-    currentColor = colors[3];
-  } else if (currentClass == "default") {
-    currentColor = "black";
-  } else {
-    currentColor = "white";
-  }
-
-  ctx.beginPath();
-}
-
-first_button.addEventListener("click", choseColor);
-second_button.addEventListener("click", choseColor);
-third_button.addEventListener("click", choseColor);
-fourth_button.addEventListener("click", choseColor);
-default_button.addEventListener("click", choseColor);
-eraser.addEventListener("click", choseColor);
-
 linewidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
