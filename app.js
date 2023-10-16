1af5fd940134c8f5bbf1366f503ca9bfc0579d71
aishatrue <140340401+aishatrue@users.noreply.github.com>
2023-10-16 20:14:29 +0900

    mode change draw

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const color = document.getElementById("color");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
+
+const modeOptions = document.getElementById("mode-button");
 // 캔버스에 그림을 그릴때 사용하는 것. canvas는 html의 한 element
 const ctx = canvas.getContext("2d");
 
@@ -12,6 +14,7 @@ canvas.height = 800;
 ctx.lineWidth = linewidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -43,8 +46,23 @@ function onColorChange(event) {
 function onColorClick(event) {
   const colorValue = event.target.dataset.color;
   ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
-
   // 선택한 색깔 알려주기 위해
+}
+
+function onModeChange(event) {
+  if (isFilling) {
+    isFilling = false;
+    modeOptions.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeOptions.innerText = "Draw";
+  }
+}
+
+function onCanvasClick(event) {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 700, 700);
+  }
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -55,7 +73,9 @@ canvas.addEventListener("mouseup", cancelPainting);
 
 canvas.addEventListener("mouseleave", cancelPainting);
 
+canvas.addEventListener("click", onCanvasClick);
 linewidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeOptions.addEventListener("click", onModeChange);
